885c673d8a4a0690f459df58c1fb4dc153712ed4
CauaYves <cauayves5@gmail.com>
2024-05-29 13:12:47 -0300

    teste

--- src/app/home/notices/[id]/page.tsx
+++ src/app/home/notices/[id]/page.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 export default function NoticeDetails({ params }: Props) {
-    console.log('teste');
+    console.log('teste2');
     return (
         <>
             <h1>edital: {params.id}</h1>
