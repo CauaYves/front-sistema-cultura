eaa2783eb8451f9d49e331ae9f84ee5f4d1e3fc8
CauaYves <cauayves5@gmail.com>
2024-08-15 13:20:35 -0300

    style: contrapartida button styled

--- src/components/organisms/MyProjects/components/externalLinks.tsx
+++ src/components/organisms/MyProjects/components/externalLinks.tsx
@@ -4,9 +4,15 @@ const LINK_CONTRAPARTIDA = 'https://form.jotform.com/241957596455069';
 export default function ExternalLinks() {
     return (
         <Paper sx={{ marginTop: '10px' }}>
-            <Box sx={{ padding: '10px' }}>
-                <Button>
-                    <Link href={LINK_CONTRAPARTIDA}>
+            <Box
+                sx={{
+                    padding: '10px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <Button variant="contained">
+                    <Link href={LINK_CONTRAPARTIDA} sx={{ color: '#FFFFFF' }}>
                         Insira a Contrapartida e Execução Cultural
                     </Link>
                 </Button>
